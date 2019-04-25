54a13aaba913f365037077575d89e618644dc9dc
DragonhandX <31867624+DragonhandX@users.noreply.github.com>
2019-04-25 14:54:34 -0400

    i don't know

---
+++ New Unity Project/Assets/startScreen.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class startScreen : MonoBehaviour {
+
+    public Canvas canvas;
+    public GameObject startButton;
+
+	// Use this for initialization
+	void Start () {
+        
+	}
+
+    private void OnMouseUpAsButton()
+    {
+        StartCoroutine(GameObject.FindObjectOfType<SceneFader>().FadeAndLoadScene(SceneFader.FadeDirection.Out, "Room 1"));
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
